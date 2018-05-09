f6764e72669e3ab15c065e9b41861ddaaef33593
realnot16 <benedetto.padula@studenti.polito.it>
2018-05-09 14:34:56 +0200

    almost done,to do SQL

--- Lab08/src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ Lab08/src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,83 @@
 package it.polito.tdp.dizionariograph;
 
+
+import it.polito.tdp.dizionariograph.model.Model;
+
+import java.util.List;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DizionarioGraphController {
-	
+	Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtLettere;
+
+    @FXML
+    private TextField txtParole;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void btGradoMax(ActionEvent event) {  	
+    	txtResult.appendText(model.findMaxDegree());
+    }
+
+    @FXML
+    void btnGraph(ActionEvent event) {
+    	int numL=0;
+    	try {
+			numL=Integer.parseInt(txtLettere.getText());
+		} catch (NumberFormatException e) {
+			txtResult.appendText("Inserire un numero valido nel campo lettere!\n");
+			return;
+		}
+    	
+    	model.createGraph(numL);
+    	txtResult.appendText("Grafo creato!\n");
+
+    }
+
+    @FXML
+    void btnReset(ActionEvent event) {
+    	txtResult.clear();
+    	txtLettere.clear();
+    	txtParole.clear();
+
+    }
+
+    @FXML
+    void btnVicini(ActionEvent event) {
+    	if(!txtParole.getText().matches("^[a-zA-z]+$")) {
+    		txtResult.appendText("Devi inserire una parola valida!\n");
+    		return;
+    	}
+    	List<String> ris=model.displayNeighbours(txtParole.getText());
+    	txtResult.appendText("I vicini della parola "+txtParole.getText()+" sono:\n");
+    	txtResult.appendText(ris.toString()+"\n");
+    }
+
+    @FXML
+    void initialize() {
+        assert txtLettere != null : "fx:id=\"txtLettere\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtParole != null : "fx:id=\"txtParole\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+    }
+    
+    public void setModel(Model model) {
+		this.model=model;
+	}
 }
+
--- Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
+++ Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
@@ -3,21 +3,72 @@ package it.polito.tdp.dizionariograph.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import it.polito.tdp.dizionariograph.db.WordDAO;
+
 public class Model {
+	Graph<String,DefaultEdge> graph;
+	List<String> parole;
 
 	public void createGraph(int numeroLettere) {
+		WordDAO wDao = new WordDAO();
+		parole=wDao.getAllWordsFixedLength(numeroLettere);  //LISTA PAROLE
+		graph = new SimpleGraph<String,DefaultEdge>(DefaultEdge.class);  //CREO GRAFO
+		Graphs.addAllVertices(graph, parole);
+		this.aggiungiVertici(numeroLettere);
+		return;
+	}
+	
+	public void aggiungiVertici(int numL) {
+		int cont=0;
+		for(String p:parole)		
+			for(String a:parole)
+				if(!a.equals(p)) {
+					
+					for(int i=0;i<numL;i++) {
+						if(cont>1){
+							cont=0;
+							break;
+						}
+						if(a.charAt(i)!=p.charAt(i))
+							cont++;
+					}
+					if(cont==1) {
+						graph.addEdge(p, a);
+					}
+						
+				}
 
-		System.err.println("createGraph -- TODO");
+		
+		return;
+		
 	}
 
 	public List<String> displayNeighbours(String parolaInserita) {
+		List<String> neighbour=new ArrayList<String>();
+		for(String p:parole)
+			if(graph.containsEdge(parolaInserita, p))
+				neighbour.add(p);
 
-		System.err.println("displayNeighbours -- TODO");
-		return new ArrayList<String>();
+		
+		return neighbour;
 	}
 
-	public int findMaxDegree() {
-		System.err.println("findMaxDegree -- TODO");
-		return -1;
+	public String findMaxDegree() {
+		int grado=0;
+		String sol="Non trovata";
+		for(String p:parole) {
+			if(grado<graph.outDegreeOf(p)) {
+				grado=graph.outDegreeOf(p);
+				sol=p;
+			}
+				
+		}
+		
+		return "Parola: "+sol+"\nGrado: "+grado+"\nVicini: "+this.displayNeighbours(sol);
 	}
 }
